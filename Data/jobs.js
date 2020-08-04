9dfd0b8c65d9aba0fbe481ddc1b466d641097ad7
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2020-08-04 18:50:35 +0300

    seconde

---
+++ Data/jobs.js
@@ -0,0 +1,30 @@
+export const jobPositions = {
+  "1a2b3c": {
+    logo: "",
+    name: "Tax Accountant",
+    company: "Singer Burke",
+    location: "Los Angeles, CA",
+    type: "full-time",
+    benefits: "none",
+    remote: true,
+    note: "",
+    minPay: "100,000",
+    maxPay: "150,000",
+    timeRangePay: "Annually",
+    jobDescription: "Working with money $$$$"
+  },
+  "4a3bbc": {
+    logo: "",
+    name: "Singer",
+    company: "Beatles",
+    location: "Los Angeles, CA",
+    type: "full-time",
+    benefits: "none",
+    remote: true,
+    note: "",
+    minPay: "10,000",
+    maxPay: "15,000",
+    timeRangePay: "Annually",
+    jobDescription: "Best singer"
+  }
+};

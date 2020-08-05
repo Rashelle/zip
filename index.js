769784b41781feb057c1fcc80c711bb0ff0a92fe
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2020-08-05 12:35:55 +0300

    added icons

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
 const LOCAL_PORT = 8080
+const PUBLIC_PATH = path.join(__dirname, 'src/public')
+app.use("/", express.static(PUBLIC_PATH));
 
-router.unsubscribe('/', express.static('/src/public/'))
+// express.static("/", "src/public");
 
-express.lister(port, () => { console.log(`listening on port ${ LOCAL_PORT}`)})
+// router.use('/', express.static('/src/public/'))
+
+// express.listen(LOCAL_PORT, () => {
+//   console.log(`listening on port ${LOCAL_PORT}`);
+// });
